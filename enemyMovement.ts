f2392242782268b29e6295274f34a302750ff0b2
JakubHruska <71689854+JakubHruska@users.noreply.github.com>
2021-04-07 11:40:13 +0200

    Backup_loaded

--- enemyMovement.ts
+++ enemyMovement.ts
@@ -1,44 +1,44 @@
-// //enemy movement
-// game.onUpdate(function () {
-//     for (let value of sprites.allOfKind(SpriteKind.Enemy)) {
-//         if (value.vx < 0) {
-//             value.setImage(assets.image`enemy`)
-//         }
-//         else if (value.vx > 0) {
-//             value.setImage(assets.image`enemy0`)
-//         }
-//         if (value.isHittingTile(CollisionDirection.Right)) {
-//             value.vx = -50
-//         }
-//         else if (value.isHittingTile(CollisionDirection.Left)) {
-//             value.vx = 50
-//         }
-//         else if (value.isHittingTile(CollisionDirection.Bottom)) {
-//             if (value.vx < 0 && (value.tileKindAt(TileDirection.Left, assets.tile`grass`))) {
-//                 value.vy = -160
-//             } else if (value.vx > 0 && value.tileKindAt(TileDirection.Right, assets.tile`grass`)) {
-//                 value.vy = -160
-//             }
-//         }
-//     }
-// })
+//enemy movement
+game.onUpdate(function () {
+    for (let value of sprites.allOfKind(SpriteKind.Enemy)) {
+        if (value.vx < 0) {
+            value.setImage(assets.image`enemy`)
+        }
+        else if (value.vx > 0) {
+            value.setImage(assets.image`enemy0`)
+        }
+        if (value.isHittingTile(CollisionDirection.Right)) {
+            value.vx = -50
+        }
+        else if (value.isHittingTile(CollisionDirection.Left)) {
+            value.vx = 50
+        }
+        else if (value.isHittingTile(CollisionDirection.Bottom)) {
+            if (value.vx < 0 && (value.tileKindAt(TileDirection.Left, assets.tile`grass`))) {
+                value.vy = -160
+            } else if (value.vx > 0 && value.tileKindAt(TileDirection.Right, assets.tile`grass`)) {
+                value.vy = -160
+            }
+        }
+    }
+})
 
-// if (currentLevel = 3) {
-//     game.onUpdate(function () {
-//         for (let value2 of sprites.allOfKind(SpriteKind.Boss)) {
-//             if (value2.vx < 0) {
-//                 value2.setImage(assets.image`bossGoose`)
-//             }
-//             else if (value2.vx > 0) {
-//                 value2.setImage(assets.image`bossGoose0`)
-//             }
+if (currentLevel = 3) {
+    game.onUpdate(function () {
+        for (let value2 of sprites.allOfKind(SpriteKind.Boss)) {
+            if (value2.vx < 0) {
+                value2.setImage(assets.image`bossGoose`)
+            }
+            else if (value2.vx > 0) {
+                value2.setImage(assets.image`bossGoose0`)
+            }
             
-//             if (value2.isHittingTile(CollisionDirection.Right)) {
-//                 value2.vx = -50
-//             }
-//             else if (value2.isHittingTile(CollisionDirection.Left)) {
-//                 value2.vx = 50
-//             }
-//         }
-//     })
-// }
+            if (value2.isHittingTile(CollisionDirection.Right)) {
+                value2.vx = -50
+            }
+            else if (value2.isHittingTile(CollisionDirection.Left)) {
+                value2.vx = 50
+            }
+        }
+    })
+}
--- logic.ts
+++ logic.ts
@@ -1,96 +1,96 @@
-// //===========skok
-// controller.A.onEvent(ControllerButtonEvent.Pressed, function() {
-//     //stojí na trávě
-//     if (player.tileKindAt(TileDirection.Bottom,assets.tile`grass`)){
-//         player.vy = -200
+//===========skok
+controller.A.onEvent(ControllerButtonEvent.Pressed, function() {
+    //stojí na trávě
+    if (player.tileKindAt(TileDirection.Bottom,assets.tile`grass`)){
+        player.vy = -200
 
-//     }
-//     //stojí na jumppadu
-//     else if (player.tileKindAt(TileDirection.Bottom,assets.tile`jumpPad`)) {
-//         player.vy = -250
-//         music.beamUp.play()
-//     }
-// })
+    }
+    //stojí na jumppadu
+    else if (player.tileKindAt(TileDirection.Bottom,assets.tile`jumpPad`)) {
+        player.vy = -250
+        music.beamUp.play()
+    }
+})
 
-// //===========zabití skokem na enemy
-// sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-//     otherSprite.destroy(effects.fire, 500)
-//     if (sprite.bottom < otherSprite.y) {
-//         sprite.vy = -100
-//         info.changeScoreBy(1)
-//         music.baDing.play()
-//     }
-//     //===========enemy zasáhne hráče
-//     else {
-//         info.changeLifeBy(-1)
-//         music.bigCrash.play()
-//     }
-// })
+//===========zabití skokem na enemy
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.fire, 500)
+    if (sprite.bottom < otherSprite.y) {
+        sprite.vy = -100
+        info.changeScoreBy(1)
+        music.baDing.play()
+    }
+    //===========enemy zasáhne hráče
+    else {
+        info.changeLifeBy(-1)
+        music.bigCrash.play()
+    }
+})
 
-// //===========zabití střelbou
-// sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
-//     sprite.destroy(effects.halo, 500)
-//     otherSprite.destroy()
-//     info.changeScoreBy(1)
-//     music.baDing.play()
-// })
+//===========zabití střelbou
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
+    sprite.destroy(effects.halo, 500)
+    otherSprite.destroy()
+    info.changeScoreBy(1)
+    music.baDing.play()
+})
 
-// //==========add HP
-// function addHP(hp: number) {
-//     if (info.life() < 5) {
-//         info.changeLifeBy(hp)
-//     }
-// }
+//==========add HP
+function addHP(hp: number) {
+    if (info.life() < 5) {
+        info.changeLifeBy(hp)
+    }
+}
 
-// //==========add ammo
-// function addAmmo(add: number) {
-//     if (ammo < 30) {
-//         ammo += add
-//     }
-// }
-// //==========max ammo, max health
-// game.onUpdate(function() {
-//     while (ammo > 30) {
-//         ammo --
-//     }
-//     while (info.life() > 5) {
-//         info.changeLifeBy(-1)
-//     }
-// })
+//==========add ammo
+function addAmmo(add: number) {
+    if (ammo < 30) {
+        ammo += add
+    }
+}
+//==========max ammo, max health
+game.onUpdate(function() {
+    while (ammo > 30) {
+        ammo --
+    }
+    while (info.life() > 5) {
+        info.changeLifeBy(-1)
+    }
+})
 
-// if (currentLevel = 3) {
-//     let bossHP = 10
-//     game.onUpdateInterval(10000000, function() {
-//         //===========poškození skokem
-//         sprites.onOverlap(SpriteKind.Player, SpriteKind.Boss, function (player, boss) {
-//             if (player.bottom < boss.y) {
-//                 player.vy = -150
-//                 music.smallCrash.play()
-//                 bossHP --
-//             }
-//             //===========boss zasáhne hráče
-//             else {
-//                 info.changeLifeBy(-1)
-//                 music.bigCrash.play()
-//             }
-//         })
+if (currentLevel = 3) {
+    let bossHP = 10
+    game.onUpdateInterval(10000000, function() {
+        //===========poškození skokem
+        sprites.onOverlap(SpriteKind.Player, SpriteKind.Boss, function (player, boss) {
+            if (player.bottom < boss.y) {
+                player.vy = -150
+                music.smallCrash.play()
+                bossHP --
+            }
+            //===========boss zasáhne hráče
+            else {
+                info.changeLifeBy(-1)
+                music.bigCrash.play()
+            }
+        })
 
-//         //===========poškození střelbou
-//         sprites.onOverlap(SpriteKind.Boss, SpriteKind.Projectile, function (boss, projectile) {
-//             projectile.destroy()
-//             music.smallCrash.play()
-//             bossHP --
-//         })
-//     })
-//     game.onUpdate(function() {
-//         //console.log(bossHP)
-//         for (let boss of sprites.allOfKind(SpriteKind.Boss)) {
-//             if (bossHP <= 0) {
-//                 boss.destroy(effects.disintegrate, 2000)
-//                 info.changeScoreBy(10)
-//                 music.powerUp.play()
-//             }
-//             boss.say(bossHP)
-//         }
-//     })
-// }
+        //===========poškození střelbou
+        sprites.onOverlap(SpriteKind.Boss, SpriteKind.Projectile, function (boss, projectile) {
+            projectile.destroy()
+            music.smallCrash.play()
+            bossHP --
+        })
+    })
+    game.onUpdate(function() {
+        //console.log(bossHP)
+        for (let boss of sprites.allOfKind(SpriteKind.Boss)) {
+            if (bossHP <= 0) {
+                boss.destroy(effects.disintegrate, 2000)
+                info.changeScoreBy(10)
+                music.powerUp.play()
+            }
+            boss.say(bossHP)
+        }
+    })
+}
